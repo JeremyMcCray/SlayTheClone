cb206c6c1694a70f0d702e9a721a23c9125580db
JeremyMcCray <Jeremy.mccray1337@gmail.com>
2021-01-16 10:03:49 -0500

    added some emplty classes

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -5,13 +5,33 @@ import cardHandling.Hand;
 
 public class Player {
 
-    Deck playerDeck;
-    int mana;
-    Hand hand;
+    private Deck playerDeck;
+    private int mana;
+    private Hand hand;
 
     public Player(Deck playerDeck, int mana, Hand hand) {
         this.playerDeck = playerDeck;
         this.mana = mana;
         this.hand = hand;
+    }
+
+    public Deck getPlayerDeck() {
+        return playerDeck;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public int incManaBy1() {
+        return mana++;
+    }
+
+    public int incMana(int inc) {
+        return mana += inc;
     }
 }
---
+++ src/main/java/unitSpawning/UnitSpawner.java
@@ -0,0 +1,18 @@
+package unitSpawning;
+
+import cards.UnitCard;
+import models.Player;
+
+import java.util.ArrayList;
+
+public class UnitSpawner {
+
+    public ArrayList<UnitCard> getAllEnemiesForFloor(Player player, int floorNumber) {
+
+        return unitSpawnerDecisionMaker(player, floorNumber);
+    }
+
+    private ArrayList<UnitCard> unitSpawnerDecisionMaker(Player player, int floorNumber) {
+        return null;
+    }
+}
